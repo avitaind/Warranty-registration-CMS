ee738f703015afd9725eb9a782c8cecfae6457ad
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-04-26 10:46:40 +0530

    Admin & Customer Backend Work V1.2

--- resources/views/admin/adminProfile.blade.php
+++ resources/views/admin/adminProfile.blade.php
@@ -33,7 +33,7 @@
                                         <div class="avatar-upload">
                                             <div class="avatar-preview ec-preview">
                                                 <div class="imagePreview ec-div-preview">
-                                                    @if ($users->pic != '')
+                                                    @if (Auth::user()->pic != '')
                                                         <img class="ec-image-preview" src="{{ '/' . $users->pic }}"
                                                             alt="{{ $users->pic }}"
                                                             style="width: 50%; padding-bottom: 20px;">
