ba10732f69ad15c95593bbdff8b2dbbd68ba19f6
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-06-24 18:05:20 +0530

    Admin & Customer Backend Work V4.9

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -146,11 +146,17 @@
         <div class="page-404">
             <div class="outer">
                 <div class="middle">
-                    <div class="inner">
+                    {{-- <div class="inner">
                         <div class="inner-circle"><i class="fa fa-cogs"></i><span>500</span></div> <span
                             class="inner-status">Opps! Internal Server Error!</span> <span
                             class="inner-detail">Unfortunately we're having trouble loading the page you are looking
                             for. Please come back in a while.</span>
+                    </div> --}}
+                    <div class="inner">
+                        <div class="inner-circle"><i class="fa fa-cogs"></i><span>500</span></div> <span
+                            class="inner-status">Opps! Something Went Wrong!</span> <span
+                            class="inner-detail">Unfortunately, we're unable to register your product at this time.
+                            Please go back and check the serial number and model no.</span>
                     </div>
                 </div>
             </div>
