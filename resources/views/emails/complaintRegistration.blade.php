d32240948d52cde5b0531fca50a006771885da9f
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-08-18 16:48:44 +0530

    Complaint Registration Admin, Customer V9.0

--- resources/views/emails/complaintRegistration.blade.php
+++ resources/views/emails/complaintRegistration.blade.php
@@ -8,13 +8,14 @@
 
 <body>
 
-    <p>
-        Hi,
+    <p>Dear Valued Customer,</p>
 
-        Complaint Registration: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam officia
-        voluptate quibusdam exercitationem eligendi recusandae, nemo ipsam earum! Distinctio voluptatem expedita veniam
-        excepturi doloribus architecto aut dolor totam ratione quibusdam.
+    <p>We have received your query. It's always important for us to adhere to the needs of our customers. We endeavor to
     </p>
+    <p>quickly provide you with the solution. Thank you for bringing this up.</p>
+
+    <p>To track your query status please visit our official site <a href="https://novita-india.com/"
+            target="_blank">www.novita-india.com</a> and connect on our chatbot.</p>
 
     <p><strong>Complaint ID :</strong> {{ $complaintRegistration->ticketID }}</p>
     <p><strong>Serial Number :</strong> {{ $complaintRegistration->productSerialNo }}</p>
@@ -23,6 +24,14 @@
     {{-- <p><strong>Warranty Expiry :</strong>
         {{ date('Y-m-d', strtotime(date('Y-m-d', strtotime($complaintRegistration->purchaseDate)) . ' + 364 day')) }}
     </p> --}}
+
+    <p>Thank you for your patronage!</p>
+
+    <p>Best Regards</p>
+
+    <p>Team NOVITA</p>
+
+    <p>Please do not reply to this email. For any support email us at contact@novita-india.com</p>
 </body>
 
 </html>
