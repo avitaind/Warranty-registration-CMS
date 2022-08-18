ce2b409d974c9fe8d01fe39570685a062a40fdf3
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-08-18 13:34:03 +0530

    Complaint Registration Admin, Customer V8.8

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
 
                     <div class=" align-content-center justify-content-sm-center text-center pt-2">
                         <a href="/" title="NOVITA India" class=" text-center">
-                            <img class="" src="{{ asset('assets/img/logo/NOVITA-logo.png ') }}"
+                            <img class="" src="{{ asset('assets/img/logo/NOVITA-Logo.png ') }}"
                                 style="" alt="NOVITA-INDIA" />
                         </a>
                     </div>
