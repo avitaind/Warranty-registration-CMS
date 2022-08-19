64206e4463e0e192d8a03a18211569711a62852f
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-08-19 13:33:37 +0530

    Complaint Registration Admin, Customer V9.6

--- app/Mail/AppMailer.php
+++ app/Mail/AppMailer.php
@@ -38,7 +38,7 @@ class AppMailer
     public function sendWarrantyExtendInformation($user, Warranty_extend $warrantyExtend)
     {
         $this->to =  $warrantyExtend->user_email;
-        $this->subject = "Warranty Extend Info";
+        $this->subject = "NOVITA | Product | Warranty | Extend | Info";
         $this->view = 'emails.warrantyExtend';
         $this->data = compact('user', 'warrantyExtend');
         return $this->deliver();
@@ -49,7 +49,7 @@ class AppMailer
     public function sendWarrantyRegistrationInformation($user, Warranty_registration $WarrantyRegistration)
     {
         $this->to =  $WarrantyRegistration->user_email;
-        $this->subject = "Product Warranty Registration Info";
+        $this->subject = "NOVITA | Product | Registration | Info";
         $this->view = 'emails.WarrantyRegistration';
         $this->data = compact('user', 'WarrantyRegistration');
         return $this->deliver();
@@ -60,7 +60,7 @@ class AppMailer
     public function sendcomplaintRegistrationInformation($user, ComplaintRegistration $complaintRegistration)
     {
         $this->to =  $complaintRegistration->email;
-        $this->subject = "Complaint Registration Registration Info";
+        $this->subject = "NOVITA | Complaint | Registration | Info";
         $this->view = 'emails.complaintRegistration';
         $this->data = compact('user', 'complaintRegistration');
         return $this->deliver();
@@ -94,7 +94,7 @@ class AppMailer
     {
         // $this->to = ['bhavdeepbhardwaj555@gmail.com'];
         $this->to = $mail;
-        $this->subject = "Warranty Certificate";
+        $this->subject = "NOVITA | Product | Warranty | Certificate";
         $this->view = 'emails.certificate';
         $this->data = compact('user', 'certificate');
         return $this->deliver();
