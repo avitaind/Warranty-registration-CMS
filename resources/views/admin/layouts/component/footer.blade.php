30bcdd43583ae0b0bc897c04f506c82fb7cda4a0
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-09-20 17:47:57 +0530

    Complaint Registration Admin, Customer V11.6

--- resources/views/admin/layouts/component/footer.blade.php
+++ resources/views/admin/layouts/component/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="copyright bg-white">
         <p>
             Copyright &copy; <span id="ec-year"></span><a class="text-primary"
-            href="/" target="_blank"> NOVITA India Admin Dashboard</a>. All Rights Reserved.
+            href="/" target="_blank"> NOVITA GLOBAL Admin Dashboard</a>. All Rights Reserved.
           </p>
     </div>
 </footer>
